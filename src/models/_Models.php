b12f301ba63d597fc7bc93e76ace2e6196ab8be2
Emil Carlsson <emil-carlsson@live.se>
2019-10-10 23:23:28 +0200

    :sparkles: Add admin models

--- src/models/_Models.php
+++ src/models/_Models.php
@@ -1,8 +1,10 @@
 <?php
 
 include __DIR__ . '/Blacklisted.php';
+include __DIR__ . '/Login.php';
 include __DIR__ . '/Quote.php';
 include __DIR__ . '/RateLimiter.php';
 include __DIR__ . '/Submission.php';
 include __DIR__ . '/Token.php';
+include __DIR__ . '/User.php';
 include __DIR__ . '/XRequest.php';
