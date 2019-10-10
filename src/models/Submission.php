67383996beb5a07544061a88ce2cfa76d8beecaf
Emil Carlsson <emil-carlsson@live.se>
2019-10-10 23:22:01 +0200

    :fire: Remove the deactivation of timestamps, add fillable columns and hide 'updated_at'

--- src/models/Submission.php
+++ src/models/Submission.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Eloquent\Model as Model;
 
 class Submission extends Model {
+  // Disable timestamps
+  public $timestamps = false;
+
   // Define primary key in database
   protected $primaryKey = "id";
 
@@ -11,6 +14,12 @@ class Submission extends Model {
     'author', 
     'quote', 
     'submitted_by',
-    'ip'
+    'ip',
+    'accepted'
+  ];
+
+  // Define hidden fields not to be returned when querying database
+  protected $hidden = [
+    'updated_at'
   ];
 }
