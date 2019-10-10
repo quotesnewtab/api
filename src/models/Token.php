bc776af3facefb6b619b6671a18a7f1286066f2d
Emil Carlsson <emil-carlsson@live.se>
2019-10-10 23:21:23 +0200

    :fire: Remove the deactivation of timestamps and add 'admin' column to fillable

--- src/models/Token.php
+++ src/models/Token.php
@@ -3,15 +3,13 @@
 use Illuminate\Database\Eloquent\Model as Model;
 
 class Token extends Model {
-  // Disable timestamps
-  public $timestamps = false;
-
   // Define primary key in database
   protected $primaryKey = "id";
 
   // Define what fields are fillable
   protected $fillable = [
     'token', 
-    'comment'
+    'comment',
+    'admin'
   ];
 }
