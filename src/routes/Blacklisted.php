a4219f67d868f66f4a78cc44d00ce770d6f83d48
Emil Carlsson <emil-carlsson@live.se>
2019-10-10 23:19:45 +0200

    :sparkles: Expand routes for blacklisted

--- src/routes/Blacklisted.php
+++ src/routes/Blacklisted.php
@@ -1,6 +1,10 @@
 <?php
 
 $app->group('/blacklisted', function() {
-  $this->get('' , 'BlacklistedController:checkStatus');
-  $this->get('/', 'BlacklistedController:checkStatus');
+  $this->post  (''            , 'BlacklistedController:create'     )->add(new AuthMiddleware($this->getContainer()));
+  $this->get   (''            , 'BlacklistedController:read'       )->add(new AuthMiddleware($this->getContainer()));
+  $this->get   ('/'           , 'BlacklistedController:read'       )->add(new AuthMiddleware($this->getContainer()));
+  $this->put   ('/{id:[0-9]+}', 'BlacklistedController:update'     )->add(new AuthMiddleware($this->getContainer()));
+  $this->delete('/{id:[0-9]+}', 'BlacklistedController:delete'     )->add(new AuthMiddleware($this->getContainer()));
+  $this->get   ('/status'     , 'BlacklistedController:checkStatus')->add(new AuthMiddleware($this->getContainer()));
 })->add(new RateLimitMiddleware($container));
