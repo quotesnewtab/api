93eb9ce6d6d4db26ad12d401f249bbd664fcd731
Emil Carlsson <emil-carlsson@live.se>
2019-10-10 23:13:32 +0200

    :sparkles: Add admin controllers

--- src/controllers/_Controllers.php
+++ src/controllers/_Controllers.php
@@ -1,9 +1,21 @@
 <?php
 
 include __DIR__ . '/Controller.php';
+include __DIR__ . '/AdminController.php';
+include __DIR__ . '/AnalyticsController.php';
 include __DIR__ . '/BlacklistedController.php';
 include __DIR__ . '/QuoteController.php';
 include __DIR__ . '/SubmissionController.php';
+include __DIR__ . '/TokenController.php';
+include __DIR__ . '/UserController.php';
+
+$container['AdminController'] = function($container) {
+  return new AdminController($container);
+};
+
+$container['AnalyticsController'] = function($container) {
+  return new AnalyticsController($container);
+};
 
 $container['BlacklistedController'] = function($container) {
   return new BlacklistedController($container);
@@ -15,4 +27,12 @@ $container['QuoteController'] = function($container) {
 
 $container['SubmissionController'] = function($container) {
   return new SubmissionController($container);
+};
+
+$container['TokenController'] = function($container) {
+  return new TokenController($container);
+};
+
+$container['UserController'] = function($container) {
+  return new UserController($container);
 };
