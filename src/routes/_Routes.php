f742dcc86d0fb917686348b29c09278c28f59ba3
Emil Carlsson <emil-carlsson@live.se>
2019-10-10 23:23:23 +0200

    :sparkles: Add admin routes

--- src/routes/_Routes.php
+++ src/routes/_Routes.php
@@ -3,6 +3,12 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+// Admin routes
+require __DIR__ . '/Admin.php';
+
+// Analytics routes
+require __DIR__ . '/Analytics.php';
+
 // Blacklisted routes
 require __DIR__ . '/Blacklisted.php';
 
@@ -11,6 +17,12 @@ require __DIR__ . '/Quotes.php';
 
 // Submissions routes
 require __DIR__ . '/Submissions.php';
+
+// Tokens routes
+require __DIR__ . '/Tokens.php';
+
+// Users routes
+require __DIR__ . '/Users.php';
 
 // Cors routes
 require __DIR__ . '/Cors.php';
